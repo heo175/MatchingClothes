03b02dd4d8b37fbb97cf03ba361a22de89c6681c
heo <heo__ys@naver.com>
2020-05-05 23:17:13 +0900

    기능 완성

--- js/cpicture.js
+++ js/cpicture.js
@@ -1,9 +1,29 @@
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
+
+var i;
+var j=0;
+var slides = document.getElementsByClassName("mySlides");
+var slides2 = document.getElementsByClassName("mySlides2");
+//var dots = document.getElementsByClassName("demo");
+var captionText = document.getElementById("caption");
+
+function openModal1() {
+    document.getElementById('myModal1').style.display = "block";
+    j=1; 
+  }
+
+  function openModal2() {
+    document.getElementById('myModal2').style.display = "block"; 
+    j=2;
   }
   
-  function closeModal() {
-    document.getElementById('myModal').style.display = "none";
+  function closeModal1() {
+    document.getElementById('myModal1').style.display = "none";
+    j=0;
+  }
+
+  function closeModal2() {
+    document.getElementById('myModal2').style.display = "none";
+    j=0;
   }
   
   var slideIndex = 1;
@@ -18,19 +38,25 @@ function openModal() {
   }
   
   function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    if(j===1){
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+  
+      slides[slideIndex-1].style.display = "block";
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+
+    if(j===2){
+      if (n > slides2.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides2.length}
+      for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
+      }
+  
+      slides2[slideIndex-1].style.display = "block";
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+
+    console.log(slideIndex);
   }
